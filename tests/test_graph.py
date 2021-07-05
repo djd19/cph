3550edfbe954ff415c1ad0e57c002bb8ab4ec38d
Danilo Jr Dela Cruz <djd19@ic.ac.uk>
2021-07-05 15:44:41 +0100

    Get indices to start from 0 in graph tests.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -8,22 +8,22 @@ from cph.graph import dijkstra_dists
 def graph_tests():
     return [
     ({
-        0:[(2,5),(4,9),(5,1)],
-        1:[(3,2),(1,5)],
-        2:[(2,2),(4,6)],
-        3:[(3,6),(1,9),(5,2)],
-        4:[(4,2),(1,1)]
+        0:[(1,5),(3,9),(4,1)],
+        1:[(2,2),(0,5)],
+        2:[(1,2),(3,6)],
+        3:[(2,6),(0,9),(4,2)],
+        4:[(3,2),(0,1)]
     }, [0, 5, 7, 3, 1]),
     ({
-        0:[(2,5)],
-        1:[(4,6),(3,7)],
-        2:[(4,5)],
-        3:[(1,2)],
+        0:[(1,5)],
+        1:[(3,6),(2,7)],
+        2:[(3,5)],
+        3:[(0,2)],
     }, [0, 5, 12, 11]),
     ]
 
 
 def test_dijkstra_dists(graph_tests):
     for graph, answer in graph_tests:
-        assert dijkstra_dists(graph, 1) == answer
+        assert dijkstra_dists(graph, 0) == answer
 
